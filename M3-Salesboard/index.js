b5fe0b9f54af437696d2da2d66d639b189561e1b
Tam Nguyen <tam.nguyen@questu.ca>
2023-02-21 20:41:06 -0800

    add salesboard

--- M3-Salesboard/index.js
+++ M3-Salesboard/index.js
@@ -2,14 +2,14 @@
 let productA = {
   emoji: "⭐",
   revenue: 200,
-  commision: 50
+  commission: 50
 }
 
 // Product B info
 let productB = {
   emoji: "🔥",
   revenue: 300,
-  commision: 75
+  commission: 75
 }
 
 const starBtn = document.querySelector('.star')
@@ -18,25 +18,70 @@ const salesInput = document.querySelector('.input-livesales')
 const achiInput = document.querySelector('.input-liveachivements')
 const revenueInput = document.querySelector('.input-revenue')
 const commissionInput = document.querySelector('.input-commission')
+const resetBtn = document.querySelector('.reset')
 
 let salesRecord = []
 let achivementsRecord = []
+let totalRevenue = 0
+let totalCommission = 0
+
 
 // First achivements
 // when click star, add to live sales board productA
 // add a bell when a button is clicked for the first time
 starBtn.addEventListener('click', (e) => {
   e.preventDefault()
-  achivementsRecord.push('🔔')
-  achiInput.textContent += achivementsRecord
-  console.log(achivementsRecord)
+  salesRecord.push(productA.emoji)
+  addSales(productA)
+  renderSalesRecord(salesInput)
+  addAchivements()
 })
 
-// Second point
-// when total revenue > 2500, add a dollar bag to achivements
+fireBtn.addEventListener('click', (e) => {
+  e.preventDefault()
+  salesRecord.push(productB.emoji)
+  addSales(productB)
+  renderSalesRecord(salesInput)
+  addAchivements()
+})
 
-// Third point
-// when lengthh of sales is 15, add a trophy to achivements
+
+function renderAchivements(input) {
+  input.textContent = achivementsRecord.join('')
+}
+
+function renderSalesRecord(input) {
+  input.textContent = salesRecord.join('')
+}
+
+function addAchivements() {
+  if (salesRecord.length === 1) {
+    achivementsRecord.push('🔔')
+    renderAchivements(achiInput)
+  } else if (salesRecord.length > 15) {
+    achivementsRecord.push('🏆')
+    renderAchivements(achiInput)
+  } else if (totalRevenue > 2500) {
+    achivementsRecord.push('💰')
+    renderAchivements(achiInput)
+  }
+}
+
+function addSales(product) {
+  totalRevenue += product.revenue
+  revenueInput.textContent = `$ ${totalRevenue}`
+  totalCommission += product.commission
+  commissionInput.textContent = `$ ${totalCommission}` 
+}
 
 // Add a reset button
-
+resetBtn.addEventListener('click', function() {
+  salesRecord = []
+  achivementsRecord = []
+  totalRevenue = 0
+  totalCommission = 0
+  salesInput.textContent = ''
+  achiInput.textContent = ''
+  revenueInput.textContent = ''
+  commissionInput.textContent = ''
+})
