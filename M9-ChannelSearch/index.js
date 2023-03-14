ea1e0980e0c4d77c6a72f904bec05cb305578f75
Tam Nguyen <tam.nguyen@questu.ca>
2023-03-13 19:49:19 -0700

    finish M9 channel search

--- M9-ChannelSearch/index.js
+++ M9-ChannelSearch/index.js
@@ -1,16 +1,57 @@
-const api = 'AIzaSyAMpp5upnOEQ5RdkQuMbEVpO97je-GwWFc'
-const channelId = 'AIzaSyAMpp5upnOEQ5R'
+// Define API key and channel ID
+const apiKey = 'AIzaSyAMpp5upnOEQ5RdkQuMbEVpO97je-GwWFc';
+const channelId = 'UCmGSJVG3mCRXVOP4yZrU1Dw';
 
-// get all videos from the channelId, add try catch too and incoporate options object
-const getVideos = async () => {
-  const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyBbtIwT2evvIo19XmO1vKUphn6vjqmdOPQ&channelId=UCqZQlzSHbVJrwrn5XvzrzcA&maxResults=6&q=video`
+// Define URL for initial video search
+const initialUrl = `https://www.googleapis.com/youtube/v3/search?part=snippet&key=${apiKey}&channelId=${channelId}&maxResults=6`;
+
+// Select DOM elements
+const searchBtn = document.querySelector('.btn-search');
+const videoSection = document.querySelector('.video-section');
+const inputEl = document.querySelector('#search');
+
+// Fetch videos from API
+const getVideos = async (url) => {
   try {
-    const response = await fetch(url)
-    const data = await response.json()
-    return data.items
+    const response = await fetch(url);
+    const { items } = await response.json();
+    return items;
   } catch (err) {
-    console.error(err)
+    console.error(err);
+    return [];
   }
-}
+};
 
-document.addEventListener('click', getVideos)
+// Map videos to HTML
+const renderVideos = (videos) => {
+  const videoList = videos.map((video) => {
+    return `
+        <div class="main-card">
+          <img src="${video.snippet.thumbnails.medium.url}" alt="" srcset="">
+          <p class="main-movie-title">${video.snippet.title}</p>
+        </div>
+    `;
+  });
+  videoSection.innerHTML = videoList.join('');
+};
+
+// Search for videos based on user input
+const searchVideos = async () => {
+  const inputValue = inputEl.value;
+  const searchUrl = `https://www.googleapis.com/youtube/v3/search?part=snippet&key=${apiKey}&channelId=${channelId}&maxResults=6&q=${inputValue}`;
+  const videos = await getVideos(searchUrl);
+  renderVideos(videos);
+};
+
+// Event listener for search button
+searchBtn.addEventListener('click', searchVideos);
+
+// Initial video load on page load
+window.onload = async () => {
+  try {
+    const initialVideos = await getVideos(initialUrl);
+    renderVideos(initialVideos);
+  } catch (e) {
+    console.error(e)
+  }
+};
--- M9-YoutubeChannelSearch/index.js
+++
@@ -1,3 +0,0 @@
-const apiKey = "Your API key"
-const endpoint = "https://www.googleapis.com/youtube/v3/search"
-const channelId = "Replace with channel ID"
